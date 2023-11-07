c4ea867032bbf0de8514f155c00fc44ab9686aad
Raphant <raphaelnanje@proton.me>
2023-11-06 22:26:57 -0600

    Add new models

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -22,10 +22,12 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
-  'gpt-4-0613'
+  'gpt-4-0613',
+  'gpt-4-1106-preview',
+  'gpt-3.5-turbo-1106',
 ];
 
-export const defaultModel = 'gpt-3.5-turbo-16k';
+export const defaultModel = 'gpt-4-1106-preview';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
@@ -36,6 +38,8 @@ export const modelMaxToken = {
   'gpt-4-0613': 8192,
   'gpt-4-32k': 32768,
   'gpt-4-32k-0613': 32768,
+  'gpt-4-1106-preview': 128000,
+  'gpt-3.5-turbo-1106': 16384,
 };
 
 export const modelCost = {
@@ -70,6 +74,14 @@ export const modelCost = {
   'gpt-4-32k-0314': {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
+  },
+  'gpt-4-1106-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
+  },
+  'gpt-3.5-turbo-1106': {
+    prompt: { price: 0.0010, unit: 1000 },
+    completion: { price: 0.0020, unit: 1000 },
   },
 };
 
--- src/types/chat.ts
+++ src/types/chat.ts
@@ -49,8 +49,15 @@ export interface Folder {
   color?: string;
 }
 
-export type ModelOptions = 'gpt-4' | 'gpt-4-32k' |
-  'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' | 'gpt-3.5-turbo-0613' | 'gpt-4-0613';
+export type ModelOptions =
+  'gpt-4'
+  | 'gpt-4-32k'
+  | 'gpt-3.5-turbo'
+  | 'gpt-3.5-turbo-16k'
+  | 'gpt-3.5-turbo-0613'
+  | 'gpt-4-0613'
+  | 'gpt-4-1106-preview'
+  | 'gpt-3.5-turbo-1106';
 
 export type TotalTokenUsed = {
   [model in ModelOptions]?: {
